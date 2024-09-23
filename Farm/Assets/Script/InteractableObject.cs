41fd3d22337748785bab57015469e13a4561f252
ricemiso <e.kousei34@outlook.jp>
2024-09-23 16:30:02 +0900

    一定の範囲に入ったらアイテムを拾う(石のみ)

--- Farm/Assets/Script/InteractableObject.cs
+++ Farm/Assets/Script/InteractableObject.cs
@@ -5,9 +5,37 @@ using UnityEngine;
 public class InteractableObject : MonoBehaviour
 {
     public string ItemName;
+    public bool playerRange;
 
     public string GetItemName()
     {
         return ItemName;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && playerRange && SelectionManager.instance.onTarget) 
+        {
+            Debug.Log("Item add to inventory");
+
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerRange = false;
+        }
     }
 }
--- Farm/Assets/Script/Selection Manager.cs
+++ Farm/Assets/Script/Selection Manager.cs
@@ -7,35 +7,59 @@ using UnityEngine.UI;
 public class SelectionManager : MonoBehaviour
 {
 
+    public static SelectionManager instance { get; set; }
+
+    public bool onTarget;
     public GameObject interaction_Info_UI;
     Text interaction_text;
 
     private void Start()
     {
+        onTarget = false;
         interaction_text = interaction_Info_UI.GetComponent<Text>();
+    }
+
+    private void Awake()
+    {
+        if(instance !=null && instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
     }
 
     void Update()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
         RaycastHit hit;
+
         if (Physics.Raycast(ray, out hit))
         {
             var selectionTransform = hit.transform;
 
-            if (selectionTransform.GetComponent<InteractableObject>())
+            InteractableObject interactable = selectionTransform.GetComponent<InteractableObject>();
+
+            if (interactable && interactable.playerRange)
             {
-                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                onTarget = true;
+
+                interaction_text.text = interactable.GetItemName();
                 interaction_Info_UI.SetActive(true);
             }
             else
             {
+                onTarget = false;
                 interaction_Info_UI.SetActive(false);
             }
 
         }
         else
         {
+            onTarget = false;
             interaction_Info_UI.SetActive(false);
         }
     }
