cad32ae07a2dcd724919adaca66695d4d1162d97
ricemiso <e.kousei34@outlook.jp>
2024-09-23 18:38:34 +0900

    インベントリでのドラック＆ドロップとUIのオンオフ＆カーソルの修正

--- Farm/Assets/Script/Mouse Movement.cs
+++ Farm/Assets/Script/Mouse Movement.cs
@@ -18,18 +18,22 @@ public class MouseMovement : MonoBehaviour
 
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        xRotation -= mouseY;
+        if (!InventorySystem.Instance.isOpen)
+        {
+            float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+            float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        YRotation += mouseX;
+            xRotation -= mouseY;
 
 
-        transform.localRotation = Quaternion.Euler(xRotation, YRotation, 0f);
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+            YRotation += mouseX;
+
+
+            transform.localRotation = Quaternion.Euler(xRotation, YRotation, 0f);
+        }
 
     }
 }
