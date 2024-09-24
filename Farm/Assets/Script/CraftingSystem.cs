e36b49f09d3815b0056f2523c6694cc3048f0054
ricemiso <e.kousei34@outlook.jp>
2024-09-24 22:09:28 +0900

    木のモデルの追加と変更

--- Farm/Assets/Script/CraftingSystem.cs
+++ Farm/Assets/Script/CraftingSystem.cs
@@ -146,7 +146,7 @@ public class CraftingSystem : MonoBehaviour
 
     public IEnumerator calulate()
     {
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.1f);
 
         InventorySystem.Instance.ReCalculateList();
     }
