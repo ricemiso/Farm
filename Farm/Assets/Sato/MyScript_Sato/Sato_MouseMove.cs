9e83e91cbc911ed8dbbcabf8d69e3180e0e989ce
Sato <tkyst0117@gmail.com>
2024-10-03 14:55:01 +0900

    世界樹シーンでのプレイヤー移動

---
+++ Farm/Assets/Sato/MyScript_Sato/Sato_MouseMove.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sato_MouseMove : MonoBehaviour
+{
+
+	public float mouseSensitivity = 100f;
+
+	float xRotation = 0f;
+	float YRotation = 0f;
+
+	void Start()
+	{
+
+		Cursor.lockState = CursorLockMode.Locked;
+	}
+
+	void Update()
+	{
+
+		//if (!InventorySystem.Instance.isOpen && !CraftingSystem.Instance.isOpen)
+		{
+			float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+			float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+			xRotation -= mouseY;
+
+
+			xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+			YRotation += mouseX;
+
+
+			transform.localRotation = Quaternion.Euler(xRotation, YRotation, 0f);
+		}
+
+	}
+}
