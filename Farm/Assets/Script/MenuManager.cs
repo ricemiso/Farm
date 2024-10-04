b6dd3e35ecb5cf6066d629ba1b43b8725d2abcb2
ricemiso <e.kousei34@outlook.jp>
2024-10-05 03:24:07 +0900

    タイトルシーンの作成、メインシーンからタイトルシーンも行ける。 セーブとオプションはまだできていない

---
+++ Farm/Assets/Script/MenuManager.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager Instance { get; set; }
+
+    public GameObject menuCanvas;
+    public GameObject UICanvas;
+
+    public GameObject savemenu;
+    public GameObject settingmenu;
+    public GameObject menu;
+
+    public bool isMenuOpen;
+
+    private void Awake()
+    {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.M) && !isMenuOpen)
+        {
+            UICanvas.SetActive(false);
+            menuCanvas.SetActive(true);
+
+            isMenuOpen = true;
+
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+
+            SelectionManager.Instance.DisableSelection();
+            SelectionManager.Instance.GetComponent<SelectionManager>().enabled = false;
+
+
+        }else if (Input.GetKeyDown(KeyCode.M) && isMenuOpen)
+        {
+
+            savemenu.SetActive(false);
+            settingmenu.SetActive(false);
+            menu.SetActive(true);
+
+
+
+            UICanvas.SetActive(true);
+            menuCanvas.SetActive(false);
+
+            isMenuOpen = false;
+
+
+            if(!CraftingSystem.Instance.isOpen && !InventorySystem.Instance.isOpen)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+
+            SelectionManager.Instance.EnableSelection();
+            SelectionManager.Instance.GetComponent<SelectionManager>().enabled = true;
+        }
+    }
+}
