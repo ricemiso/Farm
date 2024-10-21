26faae0565d7f6f65f4068e4875c66f432975eee
amateurK <k.atushi.10.07@gmail.com>
2024-10-21 12:54:44 +0900

    スポナーのテスト（未使用のAiMovementを削除）

---
+++ Farm/Assets/Kakimoto/Spawner.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// 
+// 敵を召喚するクラス
+// このスクリプトをつけたオブジェクトから指定した範囲内にランダムで召喚
+// 
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+public class Spawner : MonoBehaviour
+{
+    // 敵のリスト
+    [SerializeField] List<GameObject> m_EnemyList;
+
+    // 召喚範囲（中心から範囲を示す直方体までの距離）
+    [SerializeField] Vector3 m_Range;
+
+    // テスト用
+    int counter = 0;
+    [SerializeField] int SummonTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // テスト用
+        if (++counter > SummonTime)
+        {
+            counter -= SummonTime;
+            SummonEnemy(2);
+        }
+    }
+
+    // 指定した数リスト内からランダムに敵を召喚する
+    void SummonEnemy(uint num)
+    {
+        for (uint cnt = 0; cnt < num; cnt++)
+        {
+            // 座標の決定
+            Vector3 gap;
+            gap.x = Random.Range(-m_Range.x, m_Range.x);
+            gap.y = Random.Range(-m_Range.y, m_Range.y);
+            gap.z = Random.Range(-m_Range.z, m_Range.z);
+            // 召喚
+            Instantiate(m_EnemyList[Random.Range(0, m_EnemyList.Count)],
+                gap + this.transform.position, Quaternion.identity);
+        }
+    }
+}
