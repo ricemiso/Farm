4e0386cad561f2090753aec0a383867bec0292e0
amateurK <k.atushi.10.07@gmail.com>
2024-10-30 11:44:28 +0900

    Animalたちの子クラスを追加

---
+++ Farm/Assets/Script/Animal/SupportNPC/NormalMinion.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NormalMinion : SupportAI_Movement
+{
+
+	protected override void Start()
+	{
+		base.Start();
+	}
+
+	protected override void Update()
+	{
+		base.Update();
+	}
+}
