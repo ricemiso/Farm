17067a8f4ad8fd7f5888e47dda26bcd57ac6adc3
ricemiso <e.kousei34@outlook.jp>
2024-12-04 13:56:12 +0900

    修正

--- Farm/Assets/Script/Farm/Plant.cs
+++ Farm/Assets/Script/Farm/Plant.cs
@@ -5,168 +5,167 @@ using UnityEngine;
 
 public class Plant : MonoBehaviour
 {
-	[SerializeField] GameObject seedModel;
-	[SerializeField] GameObject youngPlantModel;
-	[SerializeField] GameObject maturePlanetModel;
+    [SerializeField] GameObject seedModel;
+    [SerializeField] GameObject youngPlantModel;
+    [SerializeField] GameObject maturePlanetModel;
 
-	[SerializeField] List<GameObject> plantProduceSpawn;
+    [SerializeField] List<GameObject> plantProduceSpawn;
 
-	[SerializeField] GameObject producePrefab;
+    [SerializeField] GameObject producePrefab;
 
-	public int dayOfPlanting;
-	[SerializeField] int plantage = 0;
+    public int dayOfPlanting;
+    [SerializeField] int plantage = 0;
 
-	[SerializeField] int ageForYourModel;
-	[SerializeField] int ageForMatureModel;
-	[SerializeField] int ageForFirstProduceBatch;
+    [SerializeField] int ageForYourModel;
+    [SerializeField] int ageForMatureModel;
+    [SerializeField] int ageForFirstProduceBatch;
 
-	[SerializeField] int daysForNewProduce;
-	[SerializeField] int daysRemainingForNewProduce;
+    [SerializeField] int daysForNewProduce;
+    [SerializeField] int daysRemainingForNewProduce;
 
-	[SerializeField] bool isOneTimearvest;
-	//public bool isWatered = false;
+    [SerializeField] bool isOneTimearvest;
+    //public bool isWatered = false;
 
-	private bool hasGeneratedProduce = false;
+    private bool hasGeneratedProduce = false;
 
-	private void OnEnable()
-	{
-		TimeManager.Instance.oneDayPass.AddListener(DayPass);
-	}
+    private void OnEnable()
+    {
+        TimeManager.Instance.oneDayPass.AddListener(DayPass);
+    }
 
-	private void OnDisable()
-	{
-		TimeManager.Instance.oneDayPass.RemoveListener(DayPass);
-	}
-
-
-	private void OnDestroy()
-	{
-		Soil soil = GetComponentInParent<Soil>();
-		if (soil != null)
-		{
-			soil.isEmpty = true;
-			soil.plantName = "";
-			soil.currentplant = null;
-		}
-	}
+    private void OnDisable()
+    {
+        TimeManager.Instance.oneDayPass.RemoveListener(DayPass);
+    }
 
 
-	// 成長させる
-	public void Grow()
-	{
+    private void OnDestroy()
+    {
+        Soil soil = GetComponentInParent<Soil>();
+        if (soil != null)
+        {
+            soil.isEmpty = true;
+            soil.plantName = "";
+            soil.currentplant = null;
+        }
+    }
 
-		plantage++;
 
-		GetComponentInParent<Soil>().MakeSoilNotWatered();
+    // 成長させる
+    public void Grow()
+    {
 
-		SphereCollider collider = GetComponent<SphereCollider>();
-		if (collider != null)
-		{
-			collider.enabled = false;
-		}
+        plantage++;
 
-		CheckRroduce();
-	}
+        GetComponentInParent<Soil>().MakeSoilNotWatered();
 
-	public void DayPass()
-	{
-		if (!isOneTimearvest)
-		{
-			if (CheckGrows())
-			{
-				GenerateProduceForEmptySpawn();
-			}
+        SphereCollider collider = GetComponent<SphereCollider>();
+        if (collider != null)
+        {
+            collider.enabled = false;
+        }
+
+        CheckRroduce();
+    }
+
+    public void DayPass()
+    {
+        if (!isOneTimearvest)
+        {
+            if (CheckGrows())
+            {
+                GenerateProduceForEmptySpawn();
+            }
             else
             {
-				Grow();
-			}
+                Grow();
+            }
         }
         else
         {
-			GetComponentInParent<Soil>().MakeSoilNotWatered();
-		}
-	}
+            GetComponentInParent<Soil>().MakeSoilNotWatered();
+        }
+    }
 
     private void Update()
     {
-		private void Update()
-		{
-			if (CheckGrows() && !hasGeneratedProduce)
-			{
-				GenerateProduceForEmptySpawn();
-				hasGeneratedProduce = true; // 処理済みフラグを設定
-			}
-		}
-	}
+
+        if (CheckGrows() && !hasGeneratedProduce)
+        {
+            GenerateProduceForEmptySpawn();
+            hasGeneratedProduce = true; // 処理済みフラグを設定
+        }
+
+    }
 
     private void CheckRroduce()
-	{
-		seedModel.SetActive(plantage < ageForFirstProduceBatch);
-		youngPlantModel.SetActive(plantage >= ageForYourModel && plantage < ageForMatureModel);
-		maturePlanetModel.SetActive(plantage >= ageForMatureModel);
+    {
+        seedModel.SetActive(plantage < ageForFirstProduceBatch);
+        youngPlantModel.SetActive(plantage >= ageForYourModel && plantage < ageForMatureModel);
+        maturePlanetModel.SetActive(plantage >= ageForMatureModel);
 
 
-		if (plantage >= ageForMatureModel && isOneTimearvest)
-		{
-			MakePlantPickable();
-		}
-	}
+        if (plantage >= ageForMatureModel && isOneTimearvest)
+        {
+            MakePlantPickable();
+        }
+    }
 
-	private void MakePlantPickable()
-	{
-		GetComponent<InteractableObject>().enabled = true;
-		GetComponent<SphereCollider>().enabled = true;
-	}
-
-
-	//private void CheckGrows()
-	//{
-	//	if (plantage == ageForFirstProduceBatch)
-	//	{
-	//		GenerateProduceForEmptySpawn();
-	//	}
-
-	//	if (plantage > ageForFirstProduceBatch)
-	//	{
-	//		if (daysRemainingForNewProduce == 0)
-	//		{
-	//			GenerateProduceForEmptySpawn();
-
-	//			daysRemainingForNewProduce = daysForNewProduce;
-	//		}
-	//		else
-	//		{
-	//			daysRemainingForNewProduce--;
-	//		}
-	//	}
-	//}
-
-	// 育ち切っているならtrue
-	public bool CheckGrows()
-	{
-		if (plantage >= ageForFirstProduceBatch) return true;
-		return false;
-	}
-
-	private void GenerateProduceForEmptySpawn()
-	{
-		foreach (GameObject spawn in plantProduceSpawn)
-		{
-			if (spawn.transform.childCount == 0)
-			{
-				GameObject produce = Instantiate(producePrefab);
-				Destroy(this.gameObject,60);
-				Destroy(producePrefab, 60);
-
-				produce.transform.parent = spawn.transform;
-
-				Vector3 producePos = Vector3.zero;
-				producePos.y = 0f;
-				produce.transform.localPosition = producePos;
-
-			}
-		}
+    private void MakePlantPickable()
+    {
+        GetComponent<InteractableObject>().enabled = true;
+        GetComponent<SphereCollider>().enabled = true;
+    }
 
 
-	}
+    //private void CheckGrows()
+    //{
+    //	if (plantage == ageForFirstProduceBatch)
+    //	{
+    //		GenerateProduceForEmptySpawn();
+    //	}
+
+    //	if (plantage > ageForFirstProduceBatch)
+    //	{
+    //		if (daysRemainingForNewProduce == 0)
+    //		{
+    //			GenerateProduceForEmptySpawn();
+
+    //			daysRemainingForNewProduce = daysForNewProduce;
+    //		}
+    //		else
+    //		{
+    //			daysRemainingForNewProduce--;
+    //		}
+    //	}
+    //}
+
+    // 育ち切っているならtrue
+    public bool CheckGrows()
+    {
+        if (plantage >= ageForFirstProduceBatch) return true;
+        return false;
+    }
+
+    private void GenerateProduceForEmptySpawn()
+    {
+        foreach (GameObject spawn in plantProduceSpawn)
+        {
+            if (spawn.transform.childCount == 0)
+            {
+                GameObject produce = Instantiate(producePrefab);
+                Destroy(this.gameObject, 60);
+                Destroy(producePrefab, 60);
+
+                produce.transform.parent = spawn.transform;
+
+                Vector3 producePos = Vector3.zero;
+                producePos.y = 0f;
+                produce.transform.localPosition = producePos;
+
+            }
+        }
+
+
+    }
 }
