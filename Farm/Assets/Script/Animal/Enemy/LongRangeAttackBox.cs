888ee184d33f085e0022198547dcf9862b65ba1f
ricemiso <e.kousei34@outlook.jp>
2024-12-11 11:44:00 +0900

    したい下痢しないように修正

--- Farm/Assets/Script/Animal/Enemy/LongRangeAttackBox.cs
+++ Farm/Assets/Script/Animal/Enemy/LongRangeAttackBox.cs
@@ -32,7 +32,7 @@ public class LongRangeAttackBox : MonoBehaviour
 
     private void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("Player") || other.CompareTag("Crystal") || other.CompareTag("MiniCrystal") || other.CompareTag("SupportUnit"))
+        if (other.gameObject.GetComponent<Animal>().isDead==false && other.CompareTag("Player") || other.CompareTag("Crystal") || other.CompareTag("MiniCrystal") || other.CompareTag("SupportUnit"))
         {
             longrange.CheckAttack(other.GameObject());
         }
