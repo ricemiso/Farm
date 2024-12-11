3dd9836f60a2265af30b937dda4925e730a70c0b
Sato <tkyst0117@gmail.com>
2024-12-11 16:45:11 +0900

    ロード画面での画像貼り付け

---
+++ Farm/Assets/Sato/MyScript_Sato/SlideShow.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Numerics;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class SlideShow : MonoBehaviour
+{
+	string[] filePath;
+
+	Image img;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		//filePath[0] = "Art/LoadPicture/loadPic1.png";
+
+		//img.sprite = filePath[0];
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
