52e880bee473eaf44ce913415f54c62c02c28949
Sato <tkyst0117@gmail.com>
2024-12-11 16:12:16 +0900

    ドラゴンのクールタイム変更

--- Farm/Assets/Script/Animal/Enemy/LongRangeEnemy.cs
+++ Farm/Assets/Script/Animal/Enemy/LongRangeEnemy.cs
@@ -116,7 +116,7 @@ public class LongRange : EnemyAI_Movement
 		particle.gameObject.SetActive(false);
 		particle.Stop();
 		// 指定されたクールダウン時間待機
-		yield return new WaitForSeconds(1.0f);
+		yield return new WaitForSeconds(3.0f);
 
 		// waitを解除
 		wait = false;
