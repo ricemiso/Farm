6290cb2fd52a824630782d87abe4158b72b44cfd
Tateishi <daxianglishi@gmail.com>
2025-01-22 14:38:12 +0900

    斧の説明 & チュートリアルの文言追加

--- Farm/Assets/Script/Tutorial/ConstructionTask.cs
+++ Farm/Assets/Script/Tutorial/ConstructionTask.cs
@@ -11,7 +11,7 @@ public class ConstructionTask : ITutorialTask
 
     public string GetText()
     {
-        return "収穫したミニオンを装備して、\n左クリックで配置できます。";
+        return "収穫したミニオンを装備して、\n左クリックで配置できます。\nQキーで停止,追従の変更が可能です。";
     }
 
     public void OnTaskSetting()
